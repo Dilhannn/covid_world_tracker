dabd420185857d040039e78d08f9a9931a13b30b
Dilhan Kösekul <dilhan.kosekul@gmail.com>
2024-07-31 22:56:26 +0300

    second commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import rootReducer from './rootReducer'
-import watchFetchCountryData from './sagas';
-
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+import rootReducer from "./rootReducer";
+import watchFetchCountryData from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(sagaMiddleware)
-  );
-  
-  sagaMiddleware.run(watchFetchCountryData);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
-  export default store;
+sagaMiddleware.run(watchFetchCountryData);
+
+export default store;
